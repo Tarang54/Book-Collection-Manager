55c8c1bf2434646313c56d9a804507bdea3830b2
Tarang <tarang.sultania@classplus.co>
2025-02-06 14:11:52 +0530

    added all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-// import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BooksList from "./pages/BooksList";
+import AddBook from "./pages/AddBook";
+import Navbar from "./components/Navbar";
 
-function App() {
-  // const [count, setCount] = useState(0)
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<BooksList />} />
+      <Route path="/add" element={<AddBook />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-  return (
-    <>
-      sjoeril
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/pages/AddBook.tsx
@@ -0,0 +1,3 @@
+import BookForm from "../components/BookForm";
+const AddBook = () => <div className="p-4"><BookForm /></div>;
+export default AddBook;
